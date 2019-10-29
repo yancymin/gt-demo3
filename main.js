c84a5331f222d31d26f4000ff4ea24933b2cd18e
yancymin <senseslofter@gmail.com>
2019-10-29 16:47:58 +0800

    svg2 done

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var dots = document.querySelectorAll('.dot')
 var text = document.querySelector('#text')
 var btn = document.querySelector('#svgBtn')
 var success = document.querySelector('#success')
+var gif = document.querySelector('#sensebot')
 var tl = new TimelineMax()
 var tl3 = new TimelineMax()
 var tl33 = new TimelineMax()
@@ -51,13 +52,18 @@ function prList1Enter() {
       ease: Power3.easeOut
     })
     .to('#avatar', duration + 0.2, {
-      opacity: 1,
+      opacity: 1
     })
-    .to(success, 0.1, {
-      opacity: 1,
-      transform: 'scale(1)',
-      ease: Power3.easeOut
-    },'-=0.3')
+    .to(
+      success,
+      0.1,
+      {
+        opacity: 1,
+        transform: 'scale(1)',
+        ease: Power3.easeOut
+      },
+      '-=0.3'
+    )
     .to('#svg1SuccessPath', duration + 0.2, {
       strokeDashoffset: 0,
       ease: Power2.easeOut
@@ -83,67 +89,32 @@ function prList1Leave() {
 prList[1].addEventListener('mouseenter', prList2Enter)
 prList[1].addEventListener('mouseleave', prList2Leave)
 function prList2Enter() {
-  tl33
-    .to([svg1, svg3], duration - 0.1, {
-      opacity: 0,
-      transform: 'scale(0.95)',
-      filter: 'blur(4px)',
-      ease: Power0.easeIn
-    })
-    .to([svg1, svg3], duration, {
-      opacity: 0.6,
-      transform: 'scale(0.95)',
-      zIndex: 0,
-      ease: Power0.easeIn
-    })
-    .to(
-      svg2,
-      duration,
-      {
-        bottom: -6,
-        zIndex: 4,
-        boxShadow: '0 20px 14px rgba(71, 84, 119, 0.2)',
-        ease: Power1.easeIn
-      },
-      '-=0.4'
-    )
+  TweenMax.set(svg2, {
+    bottom: -6,
+    zIndex: 4,
+    boxShadow: '0 20px 14px rgba(71, 84, 119, 0.2)',
+    ease: Power1.easeIn
+  })
 
-  tl3
-    .to('#defaultBtn', duration, {
-      delay: 0.3,
-      opacity: 1,
-      transform: 'scale(0.96)',
-      ease: Power3.easeOut
-    })
-    .to('#defaultBtn', duration - 0.1, {
-      opacity: 0,
-      ease: Power3.easeOut,
-      transform: 'scale(1)',
-      clearProps: 'all'
-    })
-    .to('#success2', duration - 0.1, {
-      opacity: 1,
-      transform: 'scale(0.96)'
-    })
-    .to('#success2', duration + 0.3, {
-      opacity: 1,
-      transform: 'scale(1)',
-      clearProps: 'all'
-    })
+  TweenMax.set([svg1, svg3], {
+    opacity: 0.75,
+    transform: 'scale(0.95)',
+    filter: 'blur(12px)'
+  })
 
-  tl3.timeScale(0.5)
-  tl3.repeat(20)
+  //   TweenMax.set('#sensebot', {
+  //     display: 'block'
+  //   })
+
+  gif.src = 'sensebot.gif'
 }
 
 function prList2Leave() {
-  TweenMax.set([svg1, svg2, svg3, '#success2'], {
+  gif.src = 'static-pic.png'
+
+  TweenMax.set([svg1, svg2, svg3], {
     clearProps: 'all'
   })
-  TweenMax.set('#success2', {
-    opacity: 0
-  })
-
-  tl3.repeat(false)
 }
 
 prList[2].addEventListener('mouseenter', prList3Enter)
